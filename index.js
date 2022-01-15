bb94458af1c82bc13a88ac9340ebd476b354bf91
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-01-15 13:31:26 -0300

    Impl req 01

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const talkers = require('./middlewares/talkerMiddleware');
 // Iniciando o projeto;
 
 const app = express();
@@ -8,6 +8,8 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', talkers);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
---
+++ middlewares/talkerMiddleware.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const talker = (req, res) => {
+  fs.readFile('./talker.json', 'utf8', (error, data) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(200).send(JSON.parse(data));
+    }
+  });
+};
+
+module.exports = talker;
